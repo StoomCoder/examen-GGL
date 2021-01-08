f1ff840c5db52a1f4940f0c5ec7f8c25366c0bbb
Stoomcode <cesarjhe15@gmail.com>
2021-01-08 12:24:27 -0600

    form

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 const controller = {};
-
+//Controlador para listar elementos y ejecutar el query
 controller.list = (req,res)=>{
     req.getConnection((err,conn) => {
         conn.query('SELECT a.id, a.nombre, a.apellido_paterno, pc.total, spc.servicio, count(pc.total) FROM clientes a, contratos_clientes cc, pagos_cliente pc, servicios_principales_cliente spc WHERE a.id=cc.cliente_id AND cc.id=pc.contrato_id AND pc.servicio_id=spc.id group by pc.total;'
@@ -15,23 +15,10 @@ controller.list = (req,res)=>{
     });
 };
 
-
-
+//Controlador para imprimir los elementos
 controller.print = (req,res)=>{
     res.send('print');
 };
 
-
-
-
-controller.save = (req,res)=>{
-    res.send('save');
-};
-controller.update = (req,res)=>{
-    res.send('update');
-};
-controller.delete = (req,res)=>{
-    res.send('delete');
-};
 
 module.exports = controller;
